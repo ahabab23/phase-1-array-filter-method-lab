60c55babf489304333de97254228e9ff7a9e0fd7
Uwes Ahabab <uwesahabab@gmail.com>
2024-03-27 11:27:52 +0300

    Array Method

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(driver,string){
+    return driver.filter(function(driver){
+        return driver.toLowerCase()===string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase() })
+  }
+  
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
